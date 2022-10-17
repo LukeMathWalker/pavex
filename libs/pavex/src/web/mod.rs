78f66e19a4e9e253391c178e228cdd54684915e0
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-10-17 09:19:20 +0100

    Break down app.rs into smaller modules (#5)
    * Move GeneratedApp to its own module.
    
    * Move resolution to a separate module.
    
    * Fix unused imports.
    
    * Comment.
    
    * Documentation.
    
    * Documentation.
    
    * Documentation.

--- libs/pavex/src/web/mod.rs
+++ libs/pavex/src/web/mod.rs
@@ -1,10 +1,12 @@
+pub use app::App;
+pub use diagnostic::CompilerDiagnostic;
+
 mod app;
 mod application_state_call_graph;
 mod codegen;
 mod codegen_utils;
 pub(crate) mod dependency_graph;
 mod diagnostic;
+mod generated_app;
 mod handler_call_graph;
-
-pub use app::App;
-pub use diagnostic::CompilerDiagnostic;
+mod resolvers;
