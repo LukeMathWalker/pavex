ddcffae85f955e0e2587b6af61d3d7d4958ff954
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-10-30 11:34:23 +0100

    Handle references.

--- libs/pavex/src/language/resolved_type.rs
+++ libs/pavex/src/language/resolved_type.rs
@@ -52,7 +52,8 @@ impl ResolvedType {
                 )
             })
             .unwrap();
-        write!(&mut buffer, "{}", crate_name).unwrap();
+        let maybe_reference = if self.is_shared_reference { "&" } else { "" };
+        write!(&mut buffer, "{}{}", maybe_reference, crate_name).unwrap();
         write!(&mut buffer, "::{}", self.base_type[1..].join("::")).unwrap();
         if !self.generic_arguments.is_empty() {
             write!(&mut buffer, "<").unwrap();
@@ -71,7 +72,8 @@ impl ResolvedType {
 
 impl std::fmt::Debug for ResolvedType {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.base_type.join("::"))?;
+        let maybe_reference = if self.is_shared_reference { "&" } else { "" };
+        write!(f, "{}{}", maybe_reference, self.base_type.join("::"))?;
         if !self.generic_arguments.is_empty() {
             write!(f, "<")?;
             let mut arguments = self.generic_arguments.iter().peekable();
