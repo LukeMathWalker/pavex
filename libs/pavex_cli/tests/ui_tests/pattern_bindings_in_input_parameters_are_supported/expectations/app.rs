e5ebfef8523ce19b956f9fdef4249e429cd3f28b
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-12-02 09:40:45 +0000

    Update expectations (again). Damn formatting.

--- libs/pavex_cli/tests/ui_tests/pattern_bindings_in_input_parameters_are_supported/expectations/app.rs
+++ libs/pavex_cli/tests/ui_tests/pattern_bindings_in_input_parameters_are_supported/expectations/app.rs
@@ -5,13 +5,16 @@ struct ServerState {
     router: pavex_runtime::routing::Router<u32>,
     application_state: ApplicationState,
 }
+
 pub struct ApplicationState {
     s0: app::Streamer,
 }
+
 pub async fn build_application_state() -> crate::ApplicationState {
     let v0 = app::streamer();
     crate::ApplicationState { s0: v0 }
 }
+
 pub async fn run(
     server_builder: pavex_runtime::hyper::server::Builder<
         pavex_runtime::hyper::server::conn::AddrIncoming,
@@ -60,6 +63,7 @@ async fn route_request(
         _ => panic!("This is a bug, no route registered for a route id"),
     }
 }
+
 pub async fn route_handler_0(v0: app::Streamer) -> http::Response<hyper::Body> {
     app::stream_file(v0)
 }
