999877a47440d990caa2e65f144454fe6f6caeb3
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-03-02 07:47:18 +0000

    Test the error message when there are multiple underconstrained generic parameters (#24)
    We also add a label under the output type.

--- libs/pavex_cli/tests/ui_tests/constructors_input_parameters_cannot_be_generic/lib.rs
+++ libs/pavex_cli/tests/ui_tests/constructors_input_parameters_cannot_be_generic/lib.rs
@@ -6,13 +6,33 @@ pub fn generic_constructor<T>(generic_input: GenericType<T>) -> u8 {
     todo!()
 }
 
-pub fn handler(size: u8) -> pavex_runtime::response::Response {
+pub fn doubly_generic_constructor<T, S>(i1: GenericType<T>, i2: GenericType<S>) -> u16 {
+    todo!()
+}
+
+pub fn triply_generic_constructor<T, S, U>(
+    i1: GenericType<T>,
+    i2: GenericType<S>,
+    i3: GenericType<U>,
+) -> u32 {
+    todo!()
+}
+
+pub fn handler(i: u8, j: u16, k: u32) -> pavex_runtime::response::Response {
     todo!()
 }
 
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
     bp.constructor(f!(crate::generic_constructor), Lifecycle::RequestScoped);
+    bp.constructor(
+        f!(crate::doubly_generic_constructor),
+        Lifecycle::RequestScoped,
+    );
+    bp.constructor(
+        f!(crate::triply_generic_constructor),
+        Lifecycle::RequestScoped,
+    );
     bp.route(GET, "/home", f!(crate::handler));
     bp
 }
