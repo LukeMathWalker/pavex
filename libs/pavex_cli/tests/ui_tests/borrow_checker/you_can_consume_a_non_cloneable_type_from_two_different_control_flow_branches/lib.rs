323075a76ac6c08ddf46d6fbe6654db9979c4c47
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-04-13 18:43:44 +0100

    Add a test that stresses how the borrow checking behaves when branching comes into the picture. It fails for now.

---
+++ libs/pavex_cli/tests/ui_tests/borrow_checker/you_can_consume_a_non_cloneable_type_from_two_different_control_flow_branches/lib.rs
@@ -0,0 +1,56 @@
+use std::path::PathBuf;
+
+use pavex_builder::{
+    constructor::{CloningStrategy, Lifecycle},
+    f,
+    router::GET,
+    Blueprint,
+};
+use pavex_runtime::response::Response;
+
+// The call graph looks like this:
+//
+//   Result<A, E>
+//       |
+//     match
+//    /     \
+//   /       \
+// Ok(A)  B   Err(E)
+//  |    /  \  |
+// handler   error h
+//
+// The type A is not cloneable and has two consumers that take it by value (`handler` and `error handler`).
+// But those consumers are in different control flow branches, so they'll never be invoked
+// one after the other, therefore the graph is fine as is and `pavex` should accept it.
+
+pub struct A;
+
+#[derive(Debug)]
+pub struct Error;
+
+pub struct B;
+
+pub fn a() -> Result<A, Error> {
+    todo!()
+}
+
+pub fn error_handler(_e: &Error, _b: B) -> Response {
+    todo!()
+}
+
+pub fn b() -> B {
+    todo!()
+}
+
+pub fn handler(_a: A, _b: B) -> Response {
+    todo!()
+}
+
+pub fn blueprint() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.constructor(f!(crate::a), Lifecycle::RequestScoped)
+        .error_handler(f!(crate::error_handler));
+    bp.constructor(f!(crate::b), Lifecycle::RequestScoped);
+    bp.route(GET, "/home", f!(crate::handler));
+    bp
+}
