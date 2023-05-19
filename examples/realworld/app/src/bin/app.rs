cbec0b7fcbf466d1cb6482e70ec55e74800593e5
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-05-19 11:04:37 +0200

    [Realworld example] Hierarchical configuration  (#56)
    We deviate slightly from the [twelve-factor
    app](https://12factor.net/config) recommendations when it comes to
    configuration management.
    
    We use a mix of configuration files and environment variables.
    The configuration files that are checked into the repository are used
    for non-sensitive configuration values, while env variables can be used
    to override the default on a per-environment basis and to store
    sensitive values.
    
    Config management is very sensitive to the infrastructural choices made
    _around_ the application (e.g. you might be injecting config files at
    startup time, or fetching values from something like Vault), therefore
    we don't "prescribe" what it needs to look like.
    
    We use `figment` instead of `config` since it provides better error
    messages, although it's not what I'd consider the "end game" in terms of
    configuration management.

--- examples/realworld/app/src/bin/app.rs
+++ examples/realworld/app/src/bin/app.rs
@@ -5,5 +5,7 @@ async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber("realworld".into(), "info".into(), std::io::stdout);
     init_telemetry(subscriber)?;
 
+    let _configuration = app::configuration::load_configuration()?;
+
     Ok(())
 }
--- examples/realworld/app/src/lib.rs
+++ examples/realworld/app/src/lib.rs
@@ -1,1 +1,2 @@
+pub mod configuration;
 pub mod telemetry;
