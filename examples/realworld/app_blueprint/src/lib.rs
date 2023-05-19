1f54b32123999dfb673907b8b529819427c13635
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-05-19 11:11:21 +0200

    [Realworld example] Add a ping route. (#58)
    A simple "the app is reachable" endpoint that can be used as a shallow
    healthcheck.

--- examples/realworld/app_blueprint/src/lib.rs
+++ examples/realworld/app_blueprint/src/lib.rs
@@ -1,5 +1,12 @@
-use pavex_builder::Blueprint;
+use pavex_builder::{Blueprint, router::GET, f};
+use pavex_runtime::hyper::StatusCode;
+
+pub fn ping() -> StatusCode {
+    StatusCode::OK
+}
 
 pub fn app_blueprint() -> Blueprint {
-    Blueprint::new()
+    let mut bp = Blueprint::new();
+    bp.route(GET, "/api/ping", f!(crate::ping));
+    bp
 }
