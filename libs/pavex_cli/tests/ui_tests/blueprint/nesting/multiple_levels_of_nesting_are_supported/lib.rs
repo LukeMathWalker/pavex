f3e8e6769ad2a577b061f39d942bf8e5ae66c1ed
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-06-05 10:04:55 +0100

    [Fix] Ensure that deeply nested blueprints are processed correctly  (#68)
    The nested blueprint processing logic had a bug: it didn't recursively
    process doubly-nested blueprints.
    This has now been fixed and we have a regression test to make sure it
    doesn't happen again.

---
+++ libs/pavex_cli/tests/ui_tests/blueprint/nesting/multiple_levels_of_nesting_are_supported/lib.rs
@@ -0,0 +1,46 @@
+use std::path::PathBuf;
+
+use pavex_builder::{constructor::Lifecycle, f, router::GET, Blueprint};
+
+pub fn blueprint() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.constructor(f!(crate::first), Lifecycle::RequestScoped);
+    bp.nest_at("/first", first_bp());
+    bp
+}
+
+fn first_bp() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.constructor(f!(crate::second), Lifecycle::RequestScoped);
+    bp.nest_at("/second", second_bp());
+    bp
+}
+
+fn second_bp() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.constructor(f!(crate::third), Lifecycle::RequestScoped);
+    bp.nest_at("/third", third_bp());
+    bp
+}
+
+fn third_bp() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.route(GET, "/home", f!(crate::handler));
+    bp
+}
+
+pub fn first() -> u16 {
+    todo!()
+}
+
+pub fn second(_x: u16) -> u32 {
+    todo!()
+}
+
+pub fn third(_x: u32) -> String {
+    todo!()
+}
+
+pub fn handler(_x: String) -> String {
+    todo!()
+}
