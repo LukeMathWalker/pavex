1ab2ac49ba88078520f83f0c437346aca3dc17fe
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-10-23 10:03:06 +0200

    Docs.

--- libs/pavex/src/extract/route/matched_route.rs
+++ libs/pavex/src/extract/route/matched_route.rs
@@ -1,6 +1,32 @@
 use std::fmt::Formatter;
 
 #[derive(Debug, Clone, Copy)]
+/// The route template that matched for the incoming request.
+///
+/// # Example
+///
+/// If you configure your [`Blueprint`] like this:
+///
+/// ```rust
+/// use pavex::{f, blueprint::{Blueprint, router::GET}};
+/// # use pavex::{request::RequestHead, response::Response};
+/// # fn get_home(request: RequestHead) -> Response { todo!() }
+/// # fn main() {
+/// # let mut bp = Blueprint::new();
+///
+/// bp.route(GET, "/home/:home_id", f!(crate::get_home));
+/// # }
+/// ```
+///
+/// Then [`MatchedRouteTemplate`] will be set to `/home/:home_id` for a `GET /home/123` request.
+///
+/// # Use cases
+///
+/// The primary use case for [`MatchedRouteTemplate`] is telemetry—logging, metrics, etc.  
+/// It lets you strip away the dynamic parts of the request path, thus reducing the cardinality of
+/// your metrics and making it easier to aggregate them.
+///
+/// [`Blueprint`]: crate::blueprint::Blueprint
 pub struct MatchedRouteTemplate(&'static str);
 
 impl MatchedRouteTemplate {
