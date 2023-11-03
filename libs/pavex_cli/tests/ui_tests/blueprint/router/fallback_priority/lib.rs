31506da89ce0d1a898f117df2cb06125d1b358b3
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-11-03 09:13:01 +0100

    Add fallback test.

---
+++ libs/pavex_cli/tests/ui_tests/blueprint/router/fallback_priority/lib.rs
@@ -0,0 +1,35 @@
+use pavex::blueprint::{
+    router::{GET, POST},
+    Blueprint,
+};
+use pavex::f;
+use pavex::response::Response;
+
+pub fn handler() -> pavex::response::Response {
+    todo!()
+}
+
+pub fn unauthorized() -> pavex::response::Response {
+    Response::unauthorized().box_body()
+}
+
+pub fn forbidden() -> pavex::response::Response {
+    Response::forbidden().box_body()
+}
+
+pub fn blueprint() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.nest_at("/users", {
+        let mut bp = Blueprint::new();
+        bp.route(GET, "/", f!(crate::handler));
+        bp.nest({
+            let mut bp = Blueprint::new();
+            bp.route(GET, "/id", f!(crate::handler));
+            bp.fallback(f!(crate::forbidden));
+            bp
+        });
+        bp.fallback(f!(crate::unauthorized));
+        bp
+    });
+    bp
+}
