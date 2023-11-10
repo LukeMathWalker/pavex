0e9562b0a00c7b3f8d5f88b9d642139a077a6d39
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-11-10 20:21:04 +0100

    Add support for scaffolding a new Pavex project from a template.

--- libs/pavexc/src/compiler/analyses/user_components/mod.rs
+++ libs/pavexc/src/compiler/analyses/user_components/mod.rs
@@ -1,7 +1,6 @@
 pub use processed_db::UserComponentDb;
 pub use raw_db::{UserComponent, UserComponentId};
 pub(crate) use router::Router;
-pub use router_key::RouterKey;
 pub use scope_graph::{ScopeGraph, ScopeId};
 
 mod processed_db;
