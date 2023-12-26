41dd3c8fd6503260b1df48818b8bd2a75e5091ed
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-12-26 15:44:57 +0100

    Add regression test

---
+++ libs/pavex_cli/tests/ui_tests/dependency_injection/clone_nodes_are_only_added_where_needed/lib.rs
@@ -0,0 +1,39 @@
+use std::future::IntoFuture;
+
+use pavex::blueprint::{
+    constructor::{CloningStrategy, Lifecycle},
+    router::GET,
+    Blueprint,
+};
+use pavex::f;
+use pavex::middleware::Next;
+use pavex::response::Response;
+
+#[derive(Clone)]
+pub struct Singleton;
+
+impl Singleton {
+    pub fn new() -> Singleton {
+        todo!()
+    }
+}
+
+pub fn mw<C>(s: Singleton, next: Next<C>) -> Response
+where
+    C: IntoFuture<Output = Response>,
+{
+    todo!()
+}
+
+pub fn handler(s: Singleton) -> pavex::response::Response {
+    todo!()
+}
+
+pub fn blueprint() -> Blueprint {
+    let mut bp = Blueprint::new();
+    bp.constructor(f!(crate::Singleton::new), Lifecycle::Singleton)
+        .cloning(CloningStrategy::CloneIfNecessary);
+    bp.wrap(f!(crate::mw));
+    bp.route(GET, "/", f!(crate::handler));
+    bp
+}
