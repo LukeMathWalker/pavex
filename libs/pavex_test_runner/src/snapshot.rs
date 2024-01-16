98a3baa72ff07847e016827852fc901fadf3c383
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-01-16 14:02:46 +0100

    Normalize path separator.

--- libs/pavex_test_runner/src/snapshot.rs
+++ libs/pavex_test_runner/src/snapshot.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use std::time::Duration;
 
 use console::style;
+use regex::Captures;
 use similar::{Algorithm, ChangeTag, TextDiff};
 
 fn term_width() -> usize {
@@ -33,6 +34,18 @@ impl SnapshotTest {
         let trimmed_expected = trimmed_expected.replace("\r\n", "\n");
         let actual = actual.replace("\r\n", "\n");
 
+        // Path normalization for Windows, which uses `\` instead of `/` as path separator.
+        static RE: once_cell::sync::Lazy<regex::Regex> = once_cell::sync::Lazy::new(|| {
+            regex::Regex::new(r#"(?<prefix>\[\[36;1;4m)(?<path>.*)"#).unwrap()
+        });
+        let normalizer = |c: &Captures| {
+            let prefix = c.name("prefix").unwrap().as_str();
+            let path = c.name("path").unwrap().as_str().replace("\\", "/");
+            format!("{prefix}{path}",)
+        };
+        let trimmed_expected = RE.replace_all(&trimmed_expected, normalizer);
+        let actual = RE.replace_all(&actual, normalizer);
+
         let expectation_directory = self.expectation_path.parent().unwrap();
         let last_snapshot_path = expectation_directory.join(format!(
             "{}.snap",
@@ -41,7 +54,7 @@ impl SnapshotTest {
 
         if trimmed_expected != actual {
             print_changeset(&trimmed_expected, &actual);
-            fs_err::write(last_snapshot_path, actual)
+            fs_err::write(last_snapshot_path, actual.as_ref())
                 .expect("Failed to save the actual value for a failed snapshot test");
             Err(())
         } else {
