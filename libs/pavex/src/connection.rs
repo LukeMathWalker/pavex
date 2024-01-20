afd0b62239ff3fa70bd29f5a1ddbd6132cd15893
Harry Barber <harrybarber@protonmail.com>
2024-01-20 22:26:58 +0000

    add documentation to `connection` module

--- libs/pavex/src/connection.rs
+++ libs/pavex/src/connection.rs
@@ -1,11 +1,16 @@
+//! Extract data concerning the HTTP connection.
 use std::net::SocketAddr;
 
+/// Information relating to the current undelying HTTP connection.
+///
+/// It includes the [peer address](SocketAddr).
 #[derive(Clone, Debug)]
 pub struct ConnectionInfo {
     pub(crate) peer_addr: SocketAddr,
 }
 
 impl ConnectionInfo {
+    /// Returns the peer address.
     pub fn peer_addr(&self) -> SocketAddr {
         self.peer_addr
     }
