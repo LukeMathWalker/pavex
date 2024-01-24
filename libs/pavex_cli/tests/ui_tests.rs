85076288332adff74bd1d8d6bad95a1254318525
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-01-24 09:28:17 +0100

    Use similar detection mechanism for pavexc.

--- libs/pavex_cli/tests/ui_tests.rs
+++ libs/pavex_cli/tests/ui_tests.rs
@@ -6,11 +6,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let manifest_dir = PathBuf::from_str(env!("CARGO_MANIFEST_DIR")).unwrap();
     let test_data_folder = manifest_dir.join("tests").join("ui_tests");
     let test_runtime_folder = manifest_dir.parent().unwrap().join("ui_test_envs");
-    let cli_path = get_cli_path()?;
-    run_tests(cli_path, test_data_folder, test_runtime_folder)?.exit();
+    let pavex_cli_path = get_pavex_cli_path()?;
+    let pavexc_cli_path = get_pavexc_cli_path()?;
+    run_tests(
+        pavex_cli_path,
+        pavexc_cli_path,
+        test_data_folder,
+        test_runtime_folder,
+    )?
+    .exit();
 }
 
-fn get_cli_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
+fn get_pavex_cli_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
     if let Ok(path) = std::env::var("PAVEX_TEST_CLI_PATH") {
         return Ok(PathBuf::from_str(&path)?);
     }
@@ -19,4 +26,15 @@ fn get_cli_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
     let manifest_dir = PathBuf::from_str(env!("CARGO_MANIFEST_DIR"))?;
     let target_dir = manifest_dir.parent().unwrap().join("target");
     Ok(target_dir.join(profile).join("pavex"))
+}
+
+fn get_pavexc_cli_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
+    if let Ok(path) = std::env::var("PAVEXC_TEST_CLI_PATH") {
+        return Ok(PathBuf::from_str(&path)?);
+    }
+
+    let profile = std::env::var("PAVEXC_TEST_CLI_PROFILE").unwrap_or("debug".to_string());
+    let manifest_dir = PathBuf::from_str(env!("CARGO_MANIFEST_DIR"))?;
+    let target_dir = manifest_dir.parent().unwrap().join("target");
+    Ok(target_dir.join(profile).join("pavexc"))
 }
