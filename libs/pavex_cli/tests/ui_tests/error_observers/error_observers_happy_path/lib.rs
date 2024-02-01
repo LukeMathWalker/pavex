d7cfd54d2c3adc262c9ce6059bfa696677a3c43b
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-02-01 17:16:44 +0100

    Test with two.

--- libs/pavex_cli/tests/ui_tests/error_observers/error_observers_happy_path/lib.rs
+++ libs/pavex_cli/tests/ui_tests/error_observers/error_observers_happy_path/lib.rs
@@ -37,12 +37,17 @@ pub fn error_observer(_a: A, _err: &pavex::Error) {
     todo!()
 }
 
+pub fn error_observer2(_err: &pavex::Error) {
+    todo!()
+}
+
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
     bp.constructor(f!(crate::a), Lifecycle::RequestScoped);
     bp.constructor(f!(crate::b), Lifecycle::RequestScoped)
         .error_handler(f!(crate::error_handler));
     bp.error_observer(f!(crate::error_observer));
+    bp.error_observer(f!(crate::error_observer2));
     bp.route(GET, "/home", f!(crate::handler));
     bp
 }
