7c90370089507478775223568920110b4b17ee06
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-02-03 13:15:27 +0100

    Add an error observer to the project template.

--- libs/pavexc_cli/template/{{crate_name}}/src/telemetry.rs
+++ libs/pavexc_cli/template/{{crate_name}}/src/telemetry.rs
@@ -1,6 +1,6 @@
-use pavex::request::path::MatchedPathPattern;
 use pavex::http::Version;
 use pavex::middleware::Next;
+use pavex::request::path::MatchedPathPattern;
 use pavex::request::RequestHead;
 use pavex::response::Response;
 use std::borrow::Cow;
@@ -10,8 +10,8 @@ use tracing::Instrument;
 /// A logging middleware that wraps the request pipeline in the root span.
 /// It takes care to record key information about the request and the response.
 pub async fn logger<T>(next: Next<T>, root_span: RootSpan) -> Response
-    where
-        T: IntoFuture<Output = Response>,
+where
+    T: IntoFuture<Output = Response>,
 {
     let response = next
         .into_future()
@@ -19,6 +19,37 @@ pub async fn logger<T>(next: Next<T>, root_span: RootSpan) -> Response
         .await;
     root_span.record_response_data(&response);
     response
+}
+
+/// An error observer to log error details.
+///
+/// It emits an error event and attaches information about the error to the root span.
+/// If multiple errors are observed for the same request, it will emit multiple error events
+/// but only the details of the last error will be attached to the root span.
+pub async fn log_error(e: &pavex::Error, root_span: &RootSpan) {
+    let source_chain = error_source_chain(e);
+    tracing::error!(
+        error.msg = %e,
+        error.details = ?e,
+        error.source_chain = %source_chain,
+        "An error occurred during request handling",
+    );
+    root_span.record("error.msg", tracing::field::display(e));
+    root_span.record("error.details", tracing::field::debug(e));
+    root_span.record("error.source_chain", error_source_chain(e));
+}
+
+fn error_source_chain(e: &pavex::Error) -> String {
+    use std::error::Error as _;
+    use std::fmt::Write as _;
+
+    let mut chain = String::new();
+    let mut source = e.source();
+    while let Some(s) = source {
+        let _ = writeln!(chain, "- {}", s);
+        source = s.source();
+    }
+    chain
 }
 
 /// A root span is the top-level *logical* span for an incoming request.  
@@ -31,6 +62,14 @@ pub async fn logger<T>(next: Next<T>, root_span: RootSpan) -> Response
 /// failure).  
 #[derive(Debug, Clone)]
 pub struct RootSpan(tracing::Span);
+
+impl std::ops::Deref for RootSpan {
+    type Target = tracing::Span;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 impl RootSpan {
     /// Create a new root span for the given request.
@@ -52,6 +91,9 @@ impl RootSpan {
             http.response.status_code = tracing::field::Empty,
             http.route = %matched_route,
             http.target = %request_head.target.path_and_query().map(|p| p.as_str()).unwrap_or(""),
+            error.msg = tracing::field::Empty,
+            error.details = tracing::field::Empty,
+            error.source_chain = tracing::field::Empty,
         );
         Self(span)
     }
