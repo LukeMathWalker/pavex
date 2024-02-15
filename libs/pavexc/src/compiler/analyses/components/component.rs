5a48762c991daffefee542fedc527debd5a210b3
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-02-15 19:54:33 +0200

    Merge

--- libs/pavexc/src/compiler/analyses/components/component.rs
+++ libs/pavexc/src/compiler/analyses/components/component.rs
@@ -38,35 +38,3 @@ pub(crate) struct TransformerInfo {
     /// Determine if the transformed type should be taken by value or by reference.
     pub(crate) transformation_mode: ConsumptionMode,
 }
-||||||| 495ebb2a
-=======
-use crate::compiler::analyses::components::ComponentId;
-use crate::compiler::analyses::components::{ConsumptionMode, SourceId};
-use crate::compiler::analyses::computations::ComputationId;
-use crate::compiler::analyses::user_components::{ScopeId, UserComponentId};
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-/// The item stored in [`ComponentDb`].
-pub(crate) enum Component {
-    RequestHandler {
-        user_component_id: UserComponentId,
-    },
-    WrappingMiddleware {
-        source_id: SourceId,
-    },
-    ErrorHandler {
-        source_id: SourceId,
-    },
-    ErrorObserver {
-        user_component_id: UserComponentId,
-    },
-    Constructor {
-        source_id: SourceId,
-    },
-    Transformer {
-        computation_id: ComputationId,
-        transformed_component_id: ComponentId,
-        transformation_mode: ConsumptionMode,
-        scope_id: ScopeId,
-    },
-}
