bd659971d96a2d4e7a2e349a1c47f9cbebc02027
Harry Barber <harrybarber@protonmail.com>
2024-03-31 10:00:02 +0100

    remove allocation from `doc_examples`

--- doc_examples/guide/request_data/connection/project/src/connection/peer.rs
+++ doc_examples/guide/request_data/connection/project/src/connection/peer.rs
@@ -2,6 +2,6 @@ use pavex::connection::ConnectionInfo;
 use pavex::response::Response;
 
 pub fn handler(conn: ConnectionInfo) -> Response {
-    let addr = conn.peer_addr().to_string();
+    let addr = conn.peer_addr();
     Response::ok().set_typed_body(format!("Your address is {addr}"))
 }
