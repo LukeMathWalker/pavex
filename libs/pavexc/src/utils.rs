43f79962a2b8f8652ea13ba3147d05e889a4a16a
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-04-16 09:33:20 +0200

    Don't throw away error context when converting to miette::Report.

--- libs/pavexc/src/utils.rs
+++ libs/pavexc/src/utils.rs
@@ -1,4 +1,5 @@
 use std::fmt::Write;
+use std::fmt::{Display, Formatter};
 
 /// Render a list of items separated by commas, with an "and" before the last item.
 ///
@@ -28,4 +29,23 @@ where
         }
     }
     Ok(())
+}
+
+pub fn anyhow2miette(err: anyhow::Error) -> miette::Error {
+    #[derive(Debug, miette::Diagnostic)]
+    struct InteropError(anyhow::Error);
+
+    impl Display for InteropError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            self.0.fmt(f)
+        }
+    }
+
+    impl std::error::Error for InteropError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            self.0.source()
+        }
+    }
+
+    miette::Error::from(InteropError(err))
 }
