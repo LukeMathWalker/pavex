1146b48d2fda267fd9dcaa4c3a5c8423969d0cfa
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-04-21 18:33:47 +0200

    feat: Server request id is now represented as a TypeId (#272)
    A TypeId lets us keep all the desirable properties of a UUIDv7
    (k-sortable, globally unique), with the addition of type safety (i.e.
    you can recognise at a glance, thanks to the prefix, if you're looking
    at the id for a certain resource type).
    We use `sri` as prefix for the server request id.

--- libs/pavex/src/telemetry/server_request_id.rs
+++ libs/pavex/src/telemetry/server_request_id.rs
@@ -3,9 +3,21 @@ use crate::blueprint::Blueprint;
 use crate::f;
 use crate::http::HeaderValue;
 use std::fmt::Formatter;
+use std::hash::{Hash, Hasher};
+use type_safe_id::{StaticType, TypeSafeId};
 use uuid::Uuid;
 
 /// A unique identifier generated for each incoming request.
+///
+/// It is a [TypeId](https://github.com/jetify-com/typeid).
+/// The prefix is always `sri`, followed by an underscore and UUIDv7 in base32 encoding.
+/// For example:
+///
+/// ```text
+///  sri_2x4y6z8a0b1c2d3e4f5g6h7j8k
+///  └─┘ └────────────────────────┘
+/// prefix    uuid suffix (base32)
+/// ```
 ///
 /// # Example
 ///
@@ -23,8 +35,21 @@ use uuid::Uuid;
 ///         )
 /// }
 /// ```
-#[derive(Debug, Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct ServerRequestId(Uuid);
+#[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
+pub struct ServerRequestId(TypeSafeId<SriTag>);
+
+impl Hash for ServerRequestId {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.uuid().hash(state)
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct SriTag;
+
+impl StaticType for SriTag {
+    const TYPE: &'static str = "sri";
+}
 
 impl ServerRequestId {
     /// The default constructor for [`ServerRequestId`].
@@ -32,25 +57,25 @@ impl ServerRequestId {
     /// It generates a new request id using a [UUID v7](https://datatracker.ietf.org/doc/html/draft-peabody-dispatch-new-uuid-format-04#name-uuid-version-7),
     /// with a random number and the current (UNIX) timestamp.
     pub fn generate() -> Self {
-        Self(Uuid::now_v7())
+        Self(TypeSafeId::from_uuid(Uuid::now_v7()))
     }
 
     /// Access the underlying UUID.
     pub fn inner(&self) -> Uuid {
-        self.0
+        self.0.uuid()
     }
 
     /// Return a [hyphenated representation](https://docs.rs/uuid/1.7.0/uuid/struct.Uuid.html#formatting)
     /// of [`ServerRequestId`] to be used as a [`HeaderValue`].
     pub fn header_value(&self) -> HeaderValue {
-        // The hyphenated representation of a UUID is always a valid header value.
-        format!("{self}").try_into().unwrap()
+        // It is always a valid header value.
+        self.0.to_string().try_into().unwrap()
     }
 }
 
 impl From<Uuid> for ServerRequestId {
     fn from(value: Uuid) -> Self {
-        Self(value)
+        Self(TypeSafeId::from_uuid(value))
     }
 }
 
@@ -71,5 +96,20 @@ impl ServerRequestId {
     /// The [default constructor](Self::generate) for [`ServerRequestId`].
     pub fn default_constructor() -> Constructor {
         Constructor::request_scoped(f!(super::ServerRequestId::generate))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn server_request_id() {
+        let request_id = ServerRequestId::generate();
+        let header_value = request_id.header_value();
+        let header_value = header_value.to_str().unwrap();
+        assert!(header_value.starts_with("sri_"));
+        // Prefix + `_` + UUID in base32 encoding
+        assert_eq!(header_value.len(), 3 + 1 + 26)
     }
 }
