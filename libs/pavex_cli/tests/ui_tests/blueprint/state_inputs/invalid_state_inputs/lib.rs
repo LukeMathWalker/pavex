36c849324a427e340516338e9b8fdc8f18127083
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-05-27 12:50:13 +0200

    Test invalid state inputs

--- libs/pavex_cli/tests/ui_tests/blueprint/state_inputs/invalid_state_inputs/lib.rs
+++ libs/pavex_cli/tests/ui_tests/blueprint/state_inputs/invalid_state_inputs/lib.rs
@@ -3,15 +3,21 @@ use pavex::f;
 use pavex::response::Response;
 
 #[derive(Clone)]
-pub struct A;
+pub struct B<T>(T);
 
-pub fn handler(a: &A) -> Response {
+#[derive(Clone)]
+pub struct A<'a> {
+    a: &'a str,
+}
+
+pub fn handler(a: A, b: B<String>) -> Response {
     todo!()
 }
 
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
     bp.state_input(f!(crate::A));
+    bp.state_input(f!(crate::B));
     bp.route(GET, "/", f!(crate::handler));
     bp
 }
