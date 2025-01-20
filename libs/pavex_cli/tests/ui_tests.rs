a00a61ee572184764f2d871d1c37b2cc4c1ecfb8
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-01-20 10:31:55 +0100

    chore: Add a mechanism to export a profile showing where time is being spent in the setup for UI tests

--- libs/pavex_cli/tests/ui_tests.rs
+++ libs/pavex_cli/tests/ui_tests.rs
@@ -1,15 +1,44 @@
 use pavex_test_runner::run_tests;
 use std::path::PathBuf;
+use std::process::ExitCode;
 use std::str::FromStr;
+use tracing_chrome::{ChromeLayerBuilder, FlushGuard};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Registry;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt::init();
-
+fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
+    let _guard = init_telemetry();
     let manifest_dir = PathBuf::from_str(env!("CARGO_MANIFEST_DIR")).unwrap();
     let test_runtime_folder = manifest_dir.parent().unwrap().join("ui_tests");
     let pavexc_cli_path = get_pavexc_cli_path()?;
     let pavex_cli_path = get_pavex_cli_path()?;
-    run_tests(pavex_cli_path, pavexc_cli_path, test_runtime_folder)?.exit();
+    let code = run_tests(pavex_cli_path, pavexc_cli_path, test_runtime_folder)?.exit_code();
+    Ok(code)
+}
+
+fn init_telemetry() -> Option<FlushGuard> {
+    let profiling = std::env::var("PAVEX_TEST_PROFILING").is_ok_and(|s| s == "true");
+    let mut chrome_guard = None;
+    let base = Registry::default().with(tracing_subscriber::fmt::layer());
+    if profiling {
+        let trace_filename = format!(
+            "./trace-pavex-cli-ui-tests-{}.json",
+            std::time::SystemTime::UNIX_EPOCH
+                .elapsed()
+                .unwrap()
+                .as_millis()
+        );
+        let (chrome_layer, guard) = ChromeLayerBuilder::new()
+            .file(trace_filename)
+            .include_args(true)
+            .build();
+        chrome_guard = Some(guard);
+        base.with(chrome_layer).init();
+    } else {
+        base.init();
+    }
+    chrome_guard
 }
 
 fn get_pavex_cli_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
