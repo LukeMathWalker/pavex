ec6b91470668df7d20a74d1420c177e6c925f112
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-02-15 17:11:30 +0100

    chore: Remove dependency on psql for Realworld example

--- examples/realworld/scripts/init_db.sh
+++ examples/realworld/scripts/init_db.sh
@@ -2,15 +2,10 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
-  echo >&2 "    cargo install sqlx-cli --no-default-features --features native-tls,postgres --version 0.8.0"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
   echo >&2 "to install it."
   exit 1
 fi
@@ -29,30 +24,36 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  # if a postgres container is running, print instructions to kill it and exit
+  # If a postgres container is running, print instructions to kill it and exit
   RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=conduit' --format '{{.ID}}')
   if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
     echo >&2 "there is a postgres container already running, kill it with"
     echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
     exit 1
   fi
+  CONTAINER_NAME="conduit_$(date '+%s')"
   # Launch postgres using Docker
   docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d \
-      --name "conduit_$(date '+%s')" \
+      --env POSTGRES_USER=${DB_USER} \
+      --env POSTGRES_PASSWORD=${DB_PASSWORD} \
+      --health-cmd="pg_isready -U ${DB_USER} || exit 1" \
+      --health-interval=1s \
+      --health-timeout=5s \
+      --health-retries=5 \
+      --publish "${DB_PORT}":5432 \
+      --detach \
+      --name "${CONTAINER_NAME}" \
       postgres -N 1000
       # ^ Increased maximum number of connections for testing purposes
-fi
 
-# Keep pinging Postgres until it's ready to accept commands
-until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
-done
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+  done
+fi
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
