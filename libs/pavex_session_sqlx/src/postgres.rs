3c3d241bb04885ad399bfe74b3ab89bb81ed88ad
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-03-01 08:10:40 +0100

    chore(pavex_session_sqlx): Fix doc link

--- libs/pavex_session_sqlx/src/postgres.rs
+++ libs/pavex_session_sqlx/src/postgres.rs
@@ -423,7 +423,7 @@ pub struct PostgresSessionKit {
     /// By default, it's set to [`finalize_session`].
     /// The error is handled by [`FinalizeError::into_response`].
     ///
-    /// [`FinalizeError::into_response`]: pavex_session::state::errors::FinalizeError::into_response
+    /// [`FinalizeError::into_response`]: pavex_session::errors::FinalizeError::into_response
     /// [`finalize_session`]: pavex_session::finalize_session
     pub session_finalizer: Option<PostProcessingMiddleware>,
 }
