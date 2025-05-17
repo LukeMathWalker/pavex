e4c361f8949448c67ffd0215773795b491dc7365
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-05-17 13:47:42 +0200

    Run all macro tests

--- libs/pavex_macros/tests/main.rs
+++ libs/pavex_macros/tests/main.rs
@@ -1,28 +1,6 @@
-// `trybuild` tests are incredibly slow to compile on Windows, so we skip them
-// to avoid slowing down the CI pipeline. They still run on Linux and macOS,
-// and the outcome is not really platform-dependent.
-#[cfg(not(target_os = "windows"))]
 #[test]
-fn fail() {
+fn macro_ui_tests() {
     let t = trybuild::TestCases::new();
-
-    // Tests for `#[PathParams]`
-    t.pass("tests/path_params/success/*.rs");
-    t.compile_fail("tests/path_params/fail/*.rs");
-
-    // Tests for `from!`
-    t.pass("tests/from/success/*.rs");
-    t.compile_fail("tests/from/fail/*.rs");
-
-    // Tests for `constructor!`
-    t.pass("tests/constructor/success/*.rs");
-    t.compile_fail("tests/constructor/fail/*.rs");
-
-    // Tests for `wrap!`
-    t.pass("tests/wrap/success/*.rs");
-    t.compile_fail("tests/wrap/fail/*.rs");
-
-    // Tests for `wrap!`
-    t.pass("tests/config/success/*.rs");
-    t.compile_fail("tests/config/fail/*.rs");
+    t.pass("tests/*/success/*.rs");
+    t.compile_fail("tests/*/fail/*.rs");
 }
