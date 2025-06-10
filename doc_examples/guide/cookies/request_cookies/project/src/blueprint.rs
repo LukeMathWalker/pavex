8b1e5d86098a775e62144c0b86ee46aaabcda6bf
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-06-10 15:48:15 +0200

    chore: Fix cookies doc examples

--- doc_examples/guide/cookies/request_cookies/project/src/blueprint.rs
+++ doc_examples/guide/cookies/request_cookies/project/src/blueprint.rs
@@ -1,13 +1,10 @@
-use pavex::blueprint::Blueprint;
+use pavex::blueprint::{from, Blueprint};
 use pavex::cookie::CookieKit;
-use pavex::f;
 
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
+    bp.import(from![pavex]);
     CookieKit::new().register(&mut bp);
-    bp.singleton(f!(
-        <pavex::cookie::ProcessorConfig as std::default::Default>::default
-    ));
     bp.prefix("/core").nest(crate::core::blueprint());
     bp.prefix("/multiple").nest(crate::multiple::blueprint());
     bp
